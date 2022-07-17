a340876aeace1ecdc8068e77f5ea99f9c73c45c1
maityamit <maityamit308@gmail.com>
2022-07-18 02:34:25 +0530

    add

---
+++ 001 Introduction to JS/003 Var,Let, const/program.js
@@ -0,0 +1,9 @@
+console.log("JS Tutorial: 01")
+
+// var -> Globally Declared
+// let -> Block Scope Variable
+
+let a = "Amit"
+const author = "Amit"
+// const -> value will be const 
+console.log(a)
