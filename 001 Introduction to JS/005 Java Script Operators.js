71c0bf74f69cdd3d1594cd5af18206d3bab92220
maityamit <maityamit308@gmail.com>
2022-07-23 19:01:21 +0530

    add

---
+++ 001 Introduction to JS/005 Java Script Operators.js
@@ -0,0 +1,55 @@
+// 
+console.log("Operator in JS")
+
+
+// Arithmetic Operator
+let a = 45
+let b = 4
+console.log("a + b = ",a+b)
+console.log("a - b = ",a-b)
+console.log("a x b = ",a*b)
+console.log("a / b = ",a/b)
+console.log("a ** b = ",a**b)
+console.log("a % b = ",a%b)
+console.log("a ++ = ",a++) // post increment
+console.log("a -- = ",a--) // post decrement
+console.log("++ a = ",++a) // Pre incremenet
+console.log("-- a = ",--a) // pre decrement
+
+//Assignment Operator
+let v = 10
+v+=5
+console.log(v)
+v**=v;
+console.log(v)
+
+// Comparison Operator 
+let c1 = 6
+let c2 = 7
+console.log("c1 == c2 is : ", c1==c2) // any datatype
+console.log("c1 != c2 is : ", c1!=c2)
+console.log("c1 === c2 is : ", c1===c2) // Valid only same datatype
+console.log("c1 !== c2 is : ", c1!==c2)
+// > >=
+// < <= 
+
+//Logical Operator 
+// && Logical and
+// || Logical OR
+// ! Logical NOT
+let x = 5
+let y = 6
+console.log(x<y && x==5)
+console.log(x>y || x==5)
+console.log(!true)
+
+// Bitwise Operator
+let m = 5
+let n = 6
+console.log(5&6)
+
+/* sample multiline comment
+
+hello comm
+
+*/
