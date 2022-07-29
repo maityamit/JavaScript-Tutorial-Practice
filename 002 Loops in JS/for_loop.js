db068ba5ca5b9c900b9aff3e8839343a1bed31a9
maityamit <maityamit308@gmail.com>
2022-07-30 02:49:11 +0530

    add

---
+++ 002 Loops in JS/for_loop.js
@@ -0,0 +1,28 @@
+//For loops
+
+let sum = 0
+for(let i=0;i<5;i++){
+    sum+=(i+1)
+}
+console.log(sum)
+
+
+
+
+
+
+let obj = {
+    amit: 10,
+    amit2: 20,
+    amit3: 30
+}
+
+for(let a in obj){
+    console.log(a+" "+obj[a])
+}
+
+for(let b of "Amit"){
+    console.log(b)
+}
+
+
---
+++ 002 Loops in JS/while_dowhile.js
@@ -0,0 +1,18 @@
+//While loop
+
+let sum =0
+let i = 5
+while(i>0){
+    sum+=i
+    i--
+}
+console.log(sum)
+
+
+//do while loop
+i=5
+do{
+    sum+=i
+    i--
+}while(i>0)
+console.log(sum)
